8e4093c651539e9211718c33288cbae145c1a865
Tech0ne <clement.piasco@epitech.eu>
2023-02-14 11:37:48 +0100

    Rename + remove function_list.result

--- install.sh
+++ install.sh
@@ -16,10 +16,13 @@ sudo dnf -y install python3-pip 2>/dev/null
 
 pip3 install rich
 
+echo "Requirements installed"
+
+rm -rf ~/.local/share/function-check/
 mkdir -p ~/.local/share/function-check/
 mkdir -p ~/.local/bin/
 cp gdb_script.py ~/.local/share/function-check/
-cp function_checker ~/.local/bin/
-chmod +x ~/.local/bin/function_checker
+cp epitech_function_checker ~/.local/bin/
+chmod +x ~/.local/bin/epitech_function_checker
 
 echo "Copy the sample file \"allowed_functions.json\" on your project and type the command \"function_checker {binary name}\" to make a check of your binary !"
