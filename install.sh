079e6e5051352fd77e5f570e854c4744c7edd65d
Tech0ne <clement.piasco@epitech.eu>
2023-03-04 22:51:54 +0100

    Install on the system

--- install.sh
+++ install.sh
@@ -14,15 +14,14 @@ sudo dnf -y install gdb 2>/dev/null
 sudo dnf -y install python3 2>/dev/null
 sudo dnf -y install python3-pip 2>/dev/null
 
-pip3 install rich
+sudo pip3 install rich
 
 echo "Requirements installed"
 
-rm -rf ~/.local/share/function-check/
-mkdir -p ~/.local/share/function-check/
-mkdir -p ~/.local/bin/
-cp gdb_script.py ~/.local/share/function-check/
-cp epitech_function_checker ~/.local/bin/
-chmod +x ~/.local/bin/epitech_function_checker
+rm -rf /etc/function-check/
+mkdir -p /etc/function-check/
+cp gdb_script.py /etc/function-check/
+cp epitech_function_checker /usr/bin/
+chmod +x /usr/bin/epitech_function_checker
 
 echo "Copy the sample file \"allowed_functions.json\" on your project and type the command \"function_checker {binary name}\" to make a check of your binary !"
