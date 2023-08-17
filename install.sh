f1e60632c09afcc4c341f1d415b16d004034ab3b
Tech0ne <clement.piasco@epitech.eu>
2023-08-17 14:38:03 +0200

    [FIX] Global fixing, as well as main file rework. It should work better now.

--- install.sh
+++ install.sh
@@ -2,26 +2,40 @@
 
 echo "Installing requirements"
 
-# ubuntu (class)
-sudo apt-get update 2>/dev/null
-sudo apt-get -y install gdb 2>/dev/null
-sudo apt-get -y install python3 2>/dev/null
-sudo apt-get -y install python3-pip 2>/dev/null
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+fi
 
-# fedora (berk)
-sudo dnf makecache --refresh
-sudo dnf -y install gdb 2>/dev/null
-sudo dnf -y install python3 2>/dev/null
-sudo dnf -y install python3-pip 2>/dev/null
+case "$OS" in
+    Ubuntu )
+        sudo apt-get update
+        sudo apt-get install -y gdb python3 python3-pip
+        ;;
+    Fedora )
+        sudo dnf makecache --refresh
+        sudo dnf install -y gdb python3 python3-pip
+        ;;
+    * )
+        ;;
+esac
 
-sudo pip3 install rich
+sudo pip3 install -r requirements.txt
 
 echo "Requirements installed"
 
-sudo rm -rf /etc/function-check/
-sudo mkdir -p /etc/function-check/
-sudo cp gdb_script.py /etc/function-check/
-sudo cp epitech_function_checker /usr/bin/
-sudo chmod +x /usr/bin/epitech_function_checker
+sudo cp function_checker /usr/bin/function_checker
+sudo chmod +x /usr/bin/function_checker
 
 echo "Copy the sample file \"allowed_functions.json\" on your project and type the command \"function_checker {binary name}\" to make a check of your binary !"
